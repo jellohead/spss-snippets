9fbd68a30f1b755aaf594d140038e7633c26a371
Pat Escalona <pat.escalona7@gmail.com>
2022-10-05 16:04:11 -0500

    working on crosstab-with-mean.py
    found an example to pull assign entire
    dataset to a variable then work with the
    variable.
    Going to try that.

--- crosstabs/crosstab-with-mean.py
+++ crosstabs/crosstab-with-mean.py
@@ -1,7 +1,16 @@
-import numpy as np
-import pandas as pd
 import spss
+import pandas as pd
+import numpy as np
 BEGIN PROGRAM PYTHON3.
+
+pandas.crosstab(Q4, [Q2A_1, Q2A-2])
 
 
 END PROGRAM.
+
+
+# form the cross tab
+pandas.crosstab(a, [b, c], rownames=['a'], colnames=['b', 'c'])
+
+
+crossVar = pandas.crosstab(foo, bar)
